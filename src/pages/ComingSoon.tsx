995a2696748e9088e3fb16b97e9fb2f7254a30f4
lotus-pocus <116104241+lotus-pocus@users.noreply.github.com>
2025-11-21 18:07:38 +0000

    Add login button on homepage

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function ComingSoon() {
   return (
     <div
@@ -13,7 +15,24 @@ export default function ComingSoon() {
       }}
     >
       <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>QETHING</h1>
-      <p style={{ fontSize: "1.5rem" }}>Coming Soon</p>
+      <p style={{ fontSize: "1.5rem", marginBottom: "2rem" }}>Coming Soon</p>
+
+      {/* Login Button */}
+      <Link
+        to="/preview"
+        style={{
+          padding: "12px 24px",
+          backgroundColor: "black",
+          color: "white",
+          borderRadius: "8px",
+          textDecoration: "none",
+          fontSize: "1.1rem",
+          marginTop: "20px",
+          cursor: "pointer",
+        }}
+      >
+        Publisher Login
+      </Link>
     </div>
   );
 }
